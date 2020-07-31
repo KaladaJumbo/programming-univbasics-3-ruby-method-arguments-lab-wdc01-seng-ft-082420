4c86eec634187a52abf2db2ff0d7f112f092dbf1
lively-system-6174 <wkalada1@gmail.com>
2020-07-31 03:29:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,10 +5,16 @@
 
 
 def introduction(name)
-puts "Hi, my name i"
+  puts "Hi, my name #{name}"
 end
 
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
 
+def introduction_with_language_optional(name, language = "Ruby")
+  puts "Hi, my name is #{name} and i am learning to program in #{language}."
+end
 
 
 
